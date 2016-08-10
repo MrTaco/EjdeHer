6d1400a370ea58d0e1ecf0ea9eff3435613de499
Erick Castillo <castilloherrera@yahoo.com>
2016-08-10 08:40:16 -0600

    ad

--- uso_figuras.py
+++ uso_figuras.py
@@ -1,6 +1,23 @@
 from cuadrado import Cuadrado
-mi_cuadrado_1=Cuadrado(5)
-mi_cuadrado_2=Cuadrado(10)
+from triangulo import Triangulo
+print("1)Crear Figura \n2)Salir")
+opcion=int(input(" :"))
+x=False
+if opcion==1:
+	x=True:
+while x=True:
+	if opcion==1:
+		opcfigura=input("Ingrese Figura (c-Cuadrado, t-triangulo: ")
+		if opcfigura==c:
+			valorlado=int(input("Ingrese Lado: "))
+		else:
+			valoraltura=int(input("Ingrese Altura: "))
+			valorbase=int(input("Ingrese Base: "))
+	if opcion==2: 
+		if opcfigura==c:
+			print (valorlado.imprimir())
+		else:
+			print 
 
 print ("Area: ", mi_cuadrado_1.calcular_area())
 print (mi_cuadrado_1.imprimir())
